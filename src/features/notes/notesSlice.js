790232e1fe90f41912a3326eca9b8bd47c70a3e7
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-07 18:17:31 +0700

    feat: implement fetchNotes action creator and reducers

--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -1,4 +1,10 @@
-import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import {
+    createAsyncThunk,
+    createEntityAdapter,
+    createSlice,
+} from "@reduxjs/toolkit";
+
+import server from "../../apis/server";
 
 const notesAdapter = createEntityAdapter();
 
@@ -7,10 +13,34 @@ const initialState = notesAdapter.getInitialState({
     error: null,
 });
 
+export const fetchNotes = createAsyncThunk(
+    "notes/fetchNotes",
+    async (collectionName) => {
+        const { data } = await server.get("/notes", {
+            params: { collectionName },
+        });
+        return data;
+    }
+);
+
 export const notesSlice = createSlice({
     name: "notes",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchNotes.pending, (state, _) => {
+                state.status = "pending";
+            })
+            .addCase(fetchNotes.fulfilled, (state, action) => {
+                state.status = "fulfilled";
+                notesAdapter.upsertMany(state, action.payload);
+            })
+            .addCase(fetchNotes.rejected, (state, action) => {
+                state.status = "rejected";
+                state.error = action.error.message;
+            });
+    },
 });
 
 export default notesSlice.reducer;
