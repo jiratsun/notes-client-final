5ea7c37a440c90ece8aec5a2e8032ef42f0b66ff
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-07 11:52:19 +0700

    feat: setup utility css classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
 import App from "./App";
+
+import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
 
