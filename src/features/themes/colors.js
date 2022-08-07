78ff7e8fa5653d26ccc57e1ae20b6e4a59dfbb8d
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-07 15:18:13 +0700

    feat: initialize colors object to store all colors used in this project

---
+++ src/features/themes/colors.js
@@ -0,0 +1,36 @@
+const staticYellow = "#E2C044";
+const staticBlue = "#3C91E6";
+const staticGreen = "#E2C044";
+const lightNeutral = "#373325";
+const darkNeutral = "#FFFFFF";
+
+const colors = {
+    static: {
+        primaryYellow100: staticYellow,
+        primaryYellow10: staticYellow + "1A",
+        primaryBlue100: staticBlue,
+        primaryBlue10: staticBlue + "1A",
+        primaryGreen100: staticGreen,
+        primaryGreen10: staticGreen + "1A",
+        errorRed: "#E24444",
+        neutralGray: "#CDCCC8",
+    },
+    light: {
+        neutral100: lightNeutral,
+        neutral75: lightNeutral + "BF",
+        neutral50: lightNeutral + "80",
+        neutral25: lightNeutral + "40",
+        neutral10: lightNeutral + "1A",
+        neutral5: lightNeutral + "0D",
+    },
+    dark: {
+        neutral100: darkNeutral,
+        neutral75: darkNeutral + "BF",
+        neutral50: darkNeutral + "80",
+        neutral25: darkNeutral + "40",
+        neutral10: darkNeutral + "1A",
+        neutral5: darkNeutral + "0D",
+    },
+};
+
+export default colors;
