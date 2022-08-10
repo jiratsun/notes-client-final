10aedfbe3677f62568b9f670a16cdc19578326d4
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-10 17:39:32 +0700

    refactor: move dispatching of edit note action creator inside favorite button

--- src/features/notes/NoteFavoriteButton/NoteFavoriteButton.jsx
+++ src/features/notes/NoteFavoriteButton/NoteFavoriteButton.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 import colors from "../../themes/colors";
 import noteFavoriteButtonStyles from "./NoteFavoriteButton.module.css";
+import { editNote } from "../notesSlice";
 
-const NoteFavoriteButton = ({ isFavorite, onClick }) => {
+const onClick = (dispatch, collectionName, noteId, update) => {
+    dispatch(editNote({ collectionName, noteId, update }));
+};
+
+const NoteFavoriteButton = ({ isFavorite, noteId }) => {
+    const dispatch = useDispatch();
+
     return (
-        <button className={noteFavoriteButtonStyles.btn} onClick={onClick}>
+        <button
+            className={noteFavoriteButtonStyles.btn}
+            onClick={() =>
+                onClick(dispatch, "test0", noteId, { isFavorite: !isFavorite })
+            }>
             <i
                 className={`${
                     isFavorite
