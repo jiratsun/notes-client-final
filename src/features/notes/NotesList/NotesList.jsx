588ac5628cd98f153cedfaae06936886d2c0300e
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-17 21:34:54 +0700

    feat: add scroll height logic to notes list for progress bar, add divider, add type of notes (status), fix styling and use React.memo

--- src/features/notes/NotesList/NotesList.jsx
+++ src/features/notes/NotesList/NotesList.jsx
@@ -1,12 +1,26 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Note from "./Note";
 
-import { fetchNotes, selectNoteIds, selectNoteStatus } from "./notesSlice";
+import Divider from "../../../components/Divider";
+import Note from "../Note";
 
-const NotesList = () => {
+import notesListStyles from "./NotesList.module.css";
+import { fetchNotes, selectNoteIds, selectNoteStatus } from "../notesSlice";
+
+const onScroll = (setScroll) => {
+    const top = document.querySelector(
+        `.${notesListStyles.container}`
+    ).scrollTop;
+    const outOfView =
+        document.querySelector(`.${notesListStyles.container}`).scrollHeight -
+        document.querySelector(`.${notesListStyles.container}`).clientHeight;
+    setScroll((top / outOfView) * 100);
+};
+
+const NotesList = ({ setScroll, isUncertain, sortUp }) => {
     const dispatch = useDispatch();
     const noteIds = useSelector(selectNoteIds);
+    const sortedIds = sortUp ? [...noteIds].reverse() : [...noteIds];
     const noteStatus = useSelector(selectNoteStatus);
 
     useEffect(() => {
@@ -14,16 +28,20 @@ const NotesList = () => {
     }, [dispatch, noteStatus]);
 
     return (
-        <div>
-            <ul>
-                {noteIds.map((id) => (
-                    <li key={id}>
-                        <Note id={id} />
+        <ul
+            className={notesListStyles.container}
+            onScroll={() => onScroll(setScroll)}>
+            <li>
+                <Divider />
+            </li>
+            {noteStatus === "fulfilled" &&
+                sortedIds.map((noteId) => (
+                    <li key={noteId}>
+                        <Note noteId={noteId} isUncertain={isUncertain} />
                     </li>
                 ))}
-            </ul>
-        </div>
+        </ul>
     );
 };
 
-export default NotesList;
+export default React.memo(NotesList);
