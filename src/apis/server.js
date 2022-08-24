abce78e96e1958939ff976243a6b05656922a956
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-25 00:37:47 +0700

    build: use deployed server

--- src/apis/server.js
+++ src/apis/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://notes-server-final-guest.as.r.appspot.com",
 });
 
 export default server;
